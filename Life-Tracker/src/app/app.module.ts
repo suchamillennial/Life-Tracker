471e23be0754c1f7633f458c59d1972b78c50e23
Tej <tejgidvani@hotmail.com>
2018-03-26 01:28:19 -0700

    -Add the FormsModule to the app module because TD forms are being used -Create the form using html in the template and hook up the input fields with ngModel and have a reference to the form that will be accessed as a ViewChild in the .ts -Access the form as a ViewChild, and on submit print out gathered data and clear the form

--- Life-Tracker/src/app/app.module.ts
+++ Life-Tracker/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SleepDataEditorComponent } from './sleep-tracker/sleep-data-editor/slee
     SleepDataEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
