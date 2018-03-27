e20225cac044e0954b70315094398980564f86f9
Tej <tejgidvani@hotmail.com>
2018-03-26 22:51:38 -0700

    -Set a default VALID date string in the input field and have it be able to update and being injected into the form on reset. -The getCurrentTimeStampString() helper method was useful because it used the DatePipe from ts to transform the data. Initially I was using the Date pipe in the template on the current time in millis, but this wouldn't work on reseting the form. - I could use the date:'short' pipe on listing dates in the table. This was more annoying than I would like to admit...

--- Life-Tracker/src/app/sleep-tracker/sleep-data-editor/sleep-data-editor.component.ts
+++ Life-Tracker/src/app/sleep-tracker/sleep-data-editor/sleep-data-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SleepDataService } from '../sleep-data.service';
 import { SleepDataPoint } from '../sleep-data-point.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sleep-data-editor',
@@ -13,14 +14,21 @@ export class SleepDataEditorComponent implements OnInit {
   // Create a ViewChild to monitor the form
   @ViewChild('form') form: NgForm;
   expectedSleepType: string;
+
   constructor(private sleepDataService: SleepDataService) { }
 
   ngOnInit() {
     // This should be an observable so we can view when it changes
     this.expectedSleepType = this.sleepDataService.getExpectedSleepType();
+
     console.log(`ExpectedSleepType set to: ${this.expectedSleepType}`);
   }
 
+  getCurrentTimeStampString(){
+    let dp = new DatePipe(navigator.language);
+    let p = 'y/MM/dd, hh:mm:ss';
+    return dp.transform(Date.now(), p);
+  }
   onSubmit(){
     // Get the contents of the form and print them out
     let timeStamp = this.form.value.timeStamp;
@@ -33,7 +41,10 @@ export class SleepDataEditorComponent implements OnInit {
     // Update the expectedSleepType variable to match the one in the service, since it has changed due to the add
     this.expectedSleepType = this.sleepDataService.getExpectedSleepType();
     // Clear the form after a submit and set the sleep type to whatever the expectedSleepType is
-    this.form.reset({sleepType: this.expectedSleepType});
+    
+    this.form.reset({
+      sleepType: this.expectedSleepType,
+      timeStamp: this.getCurrentTimeStampString()});
   }
 
 }
